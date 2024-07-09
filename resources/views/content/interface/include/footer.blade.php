25190bda3ae9e0ad1d9fbe80bdfefa647889094f
baghdasaryangevorg97 <baghdasaryan.gevorg.97@gmail.com>
2024-07-09 10:59:25 +0400

    add video and make footer

---
+++ resources/views/content/interface/include/footer.blade.php
@@ -0,0 +1,32 @@
+  <!-- Footer Start -->
+  <div class="container-fluid text-white-50 footer mt-3" style="background-color: #000000">
+    <div class="container ">
+        <div>
+            <div class="row g-4 d-flex justify-content-between">
+                {{-- <div class="col-lg-3 mb-3">
+                    <a href="{{ route('get-interface') }}" class="navbar-brand">
+                        <img src="{{ asset('assets/img/interface/logo.png') }}" alt="logo" width = "190" >
+                    </a>
+                </div> --}}
+                <div class="col-lg-3 d-flex w-100 justify-content-center pb-3">
+                    <div class="d-flex justify-content-end ">
+                        <a class="btn  btn-outline-secondary me-2 btn-md-square rounded-circle" href=""><i
+                                class="fab fa-twitter"></i></a>
+                        <a class="btn btn-outline-secondary me-2 btn-md-square rounded-circle" href=""><i
+                                class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-outline-secondary me-2 btn-md-square rounded-circle" href=""><i
+                                class="fab fa-youtube"></i></a>
+                        <a class="btn btn-outline-secondary btn-md-square rounded-circle" href=""><i
+                                class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 text-center text-md-start pb-4 mb-md-0 w-100 d-flex justify-content-center">
+                <span style="color:#8d0de7"><i class="fas fa-copyright me-2"></i>All right reserved.</span>
+            </div>
+        </div>
+    </div>
+</div>
+
+<a href="#" class="btn border-3 border-primary rounded-circle back-to-top"
+    style="background-color: #8d0de7; color: white"><i class="fa fa-arrow-up"></i></a>
